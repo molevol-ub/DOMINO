94febb5d8ecbf8206996d27ba2eb87ae86d0fcbf
Jose F. Sanchez <jfsanchezherrero@ub.edu>
2016-07-05 16:44:09 +0200

    Examples added and installers modified

--- files/get_files_installer_Linux.sh
+++ files/get_files_installer_Linux.sh
@@ -27,17 +27,17 @@ echo "+ Changing dir to: DOMINO git repository"
 cd $1
 
 echo "+ Copying necessary files"
-cp ."/"src"/"DOMINO_perl_code"/"*pl $DOMINO_installer_folder
-cp -r ."/"src"/"DOMINO_Qt_code $DOMINO_installer_folder"/"
+cp ."/"src"/perl/"*pl $DOMINO_installer_folder
+cp -r ."/"src"/"Qt-c++ $DOMINO_installer_folder"/"
 
 cp ."/"files"/"db_default.tar.gz $DOMINO_installer_folder
-cp ."/"files"/"*Linux.gz $DOMINO_installer_folder
 cp ."/"files"/"tarGZ_modules_Perl.tar.gz $DOMINO_installer_folder
 cp ."/"files"/"tarGZ_modules_Perl_NGSQCToolkit.tar.gz $DOMINO_installer_folder
 cp ."/"files"/"*pl $DOMINO_installer_folder
+cp ."/"files"/"fonts.tar.gz $DOMINO_installer_folder
+cp ."/"files"/"*Linux.gz $DOMINO_installer_folder
 cp ."/"files"/"*Linux.sh $DOMINO_installer_folder
 cp ."/"files"/"uninstaller.sh $DOMINO_installer_folder
-cp ."/"files"/"fonts.tar.gz $DOMINO_installer_folder
 cp ."/"files"/"Linux_libs.tar.gz $DOMINO_installer_folder
 
 cp ."/"docs"/"LOGO"/"DOMINO_icon-small.png $DOMINO_installer_folder
@@ -48,23 +48,24 @@ cp ."/"docs"/"TUTORIAL $DOMINO_installer_folder
 cp ."/"docs"/"VERSION $DOMINO_installer_folder
 
 cp ."/"Change.log $DOMINO_installer_folder
-cp ."/"LICENSE.txt $DOMINO_installer_folder
-cp ."/"README $DOMINO_installer_folder
+cp ."/"LICENSE $DOMINO_installer_folder
+cp ."/"README.md $DOMINO_installer_folder
 cp ."/"NEWS $DOMINO_installer_folder
 
 mkdir $DOMINO_installer_folder"/"example
 cp ."/"example"/"454_reads"/"*fastq $DOMINO_installer_folder"/"example
 
 cd $DOMINO_installer_folder
-cd DOMINO_Qt_code
+cd Qt-c++
 echo ""
 echo "+ Generating a Makefile for DOMINO"
 $2 DOMINO.pro
+
 make
 echo ""
 echo ""
 echo "+ Removing temporary files"
-ls $DOMINO_installer_folder"/DOMINO_Qt_code/"*.o | while read files;
+ls $DOMINO_installer_folder"/Qt-c++/"*.o | while read files;
 do
 	rm $files
 done
--- files/get_files_installer_Mac.sh
+++ files/get_files_installer_Mac.sh
@@ -27,17 +27,18 @@ echo "+ Changing dir to: DOMINO git repository"
 cd $1
 
 echo "+ Copying necessary files"
-cp ."/"src"/"DOMINO_perl_code"/"*pl $DOMINO_installer_folder
-cp -r ."/"src"/"DOMINO_Qt_code $DOMINO_installer_folder"/"
+cp ."/"src"/perl/"*pl $DOMINO_installer_folder
+cp -r ."/"src"/"Qt-c++ $DOMINO_installer_folder"/"
 
-cp ."/"files"/"*Mac.gz $DOMINO_installer_folder
 cp ."/"files"/"db_default.tar.gz $DOMINO_installer_folder
 cp ."/"files"/"tarGZ_modules_Perl.tar.gz $DOMINO_installer_folder
 cp ."/"files"/"tarGZ_modules_Perl_NGSQCToolkit.tar.gz $DOMINO_installer_folder
 cp ."/"files"/"*pl $DOMINO_installer_folder
+cp ."/"files"/"fonts.tar.gz $DOMINO_installer_folder
+cp ."/"files"/"*Mac.gz $DOMINO_installer_folder
 cp ."/"files"/"*Mac.sh $DOMINO_installer_folder
 cp ."/"files"/"*command $DOMINO_installer_folder
-cp ."/"files"/"fonts.tar.gz $DOMINO_installer_folder
+
 
 cp ."/"docs"/"LOGO"/"DOMINO_icon-small.png $DOMINO_installer_folder
 cp ."/"docs"/"LOGO"/"DOMINO_logo.png $DOMINO_installer_folder
@@ -47,15 +48,15 @@ cp ."/"docs"/"TUTORIAL $DOMINO_installer_folder
 cp ."/"docs"/"VERSION $DOMINO_installer_folder
 
 cp ."/"Change.log $DOMINO_installer_folder
-cp ."/"LICENSE.txt $DOMINO_installer_folder
-cp ."/"README $DOMINO_installer_folder
+cp ."/"LICENSE $DOMINO_installer_folder
+cp ."/"README.md $DOMINO_installer_folder
 cp ."/"NEWS $DOMINO_installer_folder
 
 mkdir $DOMINO_installer_folder"/"example
 cp ."/"example"/"454_reads"/"*fastq $DOMINO_installer_folder"/"example
 
 cd $DOMINO_installer_folder
-cd DOMINO_Qt_code
+cd Qt-c++
 echo ""
 echo "+ Generating a Makefile for DOMINO"
 $2 DOMINO.pro
@@ -64,7 +65,7 @@ make
 echo ""
 echo ""
 echo "+ Removing temporary files"
-ls $DOMINO_installer_folder"/DOMINO_Qt_code/"*.o | while read files;
+ls $DOMINO_installer_folder"/Qt-c++/"*.o | while read files;
 do
 	rm $files
 done
